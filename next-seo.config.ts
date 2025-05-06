2acbfa032a55d7efa2d97d0636bbab585364829f
sinhdevops <sinh.dev.ops@gmail.com>
2025-05-06 10:11:31 +0700

    seo web

---
+++ next-seo.config.ts
@@ -0,0 +1,30 @@
+// src/next-seo.config.ts
+const SEO = {
+    title: "Lắp Mạng Mobi Đà Nẵng - Giá Rẻ, Nhanh, Uy Tín",
+    description:
+      "Dịch vụ lắp đặt mạng wifi MobiFone tại Đà Nẵng, tốc độ cao, giá siêu rẻ. Miễn phí modem, hỗ trợ kỹ thuật 24/7.",
+    openGraph: {
+      type: "website",
+      locale: "vi_VN",
+      url: "https://https://www.lapmangmobi.store/",
+      site_name: "Lắp Mạng Mobi Đà Nẵng",
+      images: [
+        {
+          url: "https://https://www.lapmangmobi.store/logo.png",
+          width: 1200,
+          height: 630,
+          alt: "Lắp mạng wifi Mobi Đà Nẵng",
+        },
+      ],
+    },
+    additionalMetaTags: [
+      {
+        name: "keywords",
+        content:
+          "lắp mạng Đà Nẵng, wifi giá rẻ, wifi MobiFone, mạng mobi, lắp mạng mobi Đà Nẵng",
+      },
+    ],
+  };
+  
+  export default SEO;
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,16 @@ import Benefits from "./_components/benefits";
 import WhyChooseUs from "./_components/why-choose-us";
 import Services from "./_components/services";
 import Hero from "./_components/hero";
+import { NextSeo } from "next-seo";
 
 const App = () => {
   return (
     <div>
+      <NextSeo
+        title="Lắp Wifi Giá Rẻ Đà Nẵng - MobiFiber Chính Hãng"
+        description="Đăng ký lắp wifi Mobi Đà Nẵng nhanh chóng, chỉ từ 150k/tháng. Tặng thêm tháng sử dụng, hỗ trợ kỹ thuật tận nơi."
+        canonical="https://www.lapmangmobi.store/"
+      />
       <Hero />
       <Benefits />
       <WhyChooseUs />
