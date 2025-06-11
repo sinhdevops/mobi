6c75f405fc85cb14301dbf8efff6eaf32e5836df
sinhdevops <sinh.dev.ops@gmail.com>
2025-06-11 10:41:29 +0700

    fix web

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import { IMAGES } from "@/assets/images";
 
 const Header = () => {
-  const HEADERS = [
-    { display: "Trang Chủ", hash: "#" },
-    { display: "Giá Cước", hash: "#services" },
-  ];
+  // const HEADERS = [
+  //   { display: "Trang Chủ", hash: "#" },
+  //   { display: "Giá Cước", hash: "#services" },
+  // ];
 
   return (
     <header className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 shadow-md">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-4 flex justify-center items-center">
         <div className="text-2xl font-bold text-[#51baf4]">
           <Image
             src={IMAGES.logo}
@@ -18,13 +18,13 @@ const Header = () => {
             height={150}
           />
         </div>
-        <nav className="hidden md:flex space-x-8">
+        {/* <nav className="hidden md:flex space-x-8">
           {HEADERS.map((headerItems) => (
             <a href={headerItems.hash} key={headerItems.hash}>
               {headerItems.display}
             </a>
           ))}
-        </nav>
+        </nav> */}
       </div>
     </header>
   );
